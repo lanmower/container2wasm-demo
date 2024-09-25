e3da641a809d644d2edfb0b3332a189e06488c66
lanmower <almagestfraternite@gmail.com>
2024-09-25 23:48:27 +0200

    Update create-images.sh

--- create-images.sh
+++ create-images.sh
@@ -47,5 +47,6 @@ do
         fi
         split -d -b "${WASI_MAX_CHUNK}" --additional-suffix=.wasm "${DEST}/${OUTPUT_NAME}.wasm" "${DEST}/${OUTPUT_NAME}"
         rm "${DEST}/${OUTPUT_NAME}.wasm"
+        cp ./docs/* ${DEST}
     fi
 done
